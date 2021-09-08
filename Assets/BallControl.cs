1115a1cb3a8de86d21157d76f53ae20f17f8f789
Sekar Ayu Alfina Shasty <sekar.ayu.1803316@students.um.ac.id>
2021-09-08 20:56:10 +0700

    Pong Game

---
+++ Assets/BallControl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+
+    // Titik asal lintasan bola saat ini
+    private Vector2 trajectoryOrigin;
+
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+    // Untuk mengakses informasi titik asal lintasan
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+
+        float RandomDirection = Random.Range(0, 2);//menentukan nilai acak
+        if (RandomDirection < 1.0f)
+        {
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
+        }
+    }
+
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+
+        // Setelah 2 detik, berikan gaya ke bola
+        Invoke("PushBall", 2);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        trajectoryOrigin = transform.position;
+
+        // Mulai game
+        RestartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/SideWall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    public PlayerControl player;
+    [SerializeField]
+    private GameManager gameManager;
+
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        //Jika objek tersebut bernama "Ball"
+        if (anotherCollider.name == "Ball")
+        {
+            //Tambahkan skor ke pemain
+            player.IncrementScore();
+
+            //Jika skor pemain belum mencapai skor maksimal
+            if (player.Score < gameManager.maxScore)
+            {
+                //restart game setelah bola mengenai dinding
+                anotherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
